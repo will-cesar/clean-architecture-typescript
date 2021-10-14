da6e54325f8e62c03501539f28da6a4af72c96a4
will-cesar <willcesar010421@gmail.com>
2021-10-13 21:01:39 -0300

    add SavePurchases use case

---
+++ src/domain/usecases/save-purchases.ts
@@ -0,0 +1,11 @@
+export interface SavePurchases {
+  save: (purchases: SavePurchases.Params[]) => Promise<void>;
+}
+
+namespace SavePurchases {
+  export type Params = {
+    id: string;
+    date: Date;
+    value: number;
+  };
+}
