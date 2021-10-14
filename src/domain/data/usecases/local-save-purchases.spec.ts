ecdfb9476c05b10a968c27a91b858b261da3f860
will-cesar <willcesar010421@gmail.com>
2021-10-13 22:12:45 -0300

    merge tests

--- src/domain/data/usecases/local-save-purchases.spec.ts
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -41,11 +41,6 @@ describe("LocalSavePurchases", () => {
     const { cacheStore, sut } = makeSut();
     await sut.save();
     expect(cacheStore.deleteCallsCount).toBe(1);
-  });
-
-  test("Should call delete with correct key", async () => {
-    const { cacheStore, sut } = makeSut();
-    await sut.save();
     expect(cacheStore.key).toBe("purchases");
   });
 });
