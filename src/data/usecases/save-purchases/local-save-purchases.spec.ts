fa1c20c8149ac96867f70eaf5c8dd228f653958c
will-cesar <willcesar010421@gmail.com>
2021-10-16 15:14:45 -0300

    ensure LocalSavePurchases insert cache with correct key

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -4,11 +4,17 @@ import { LocalSavePurchases } from "@/data/usecases";
 class CacheStoreSpy implements CacheStore {
   deleteCallsCount = 0;
   insertCallsCount = 0;
-  key: string;
+  deleteKey: string;
+  insertKey: string;
 
   delete(key: string): void {
     this.deleteCallsCount++;
-    this.key = key;
+    this.deleteKey = key;
+  }
+
+  insert(key: string): void {
+    this.insertCallsCount++;
+    this.insertKey = key;
   }
 }
 
@@ -34,10 +40,10 @@ describe("LocalSavePurchases", () => {
     const { cacheStore, sut } = makeSut();
     await sut.save();
     expect(cacheStore.deleteCallsCount).toBe(1);
-    expect(cacheStore.key).toBe("purchases");
+    expect(cacheStore.deleteKey).toBe("purchases");
   });
 
-  test("Should not insert new Cache if delete fails", async () => {
+  test("Should not insert new Cache if delete fails", () => {
     /**
       Teste para garantir que o método insert não vai ser chamado
       caso haja um erro ao deletar o cache
@@ -53,5 +59,13 @@ describe("LocalSavePurchases", () => {
     const promise = sut.save();
     expect(cacheStore.insertCallsCount).toBe(0);
     expect(promise).rejects.toThrow();
+  });
+
+  test("Should insert new Cache if delete succeeds", async () => {
+    const { cacheStore, sut } = makeSut();
+    await sut.save();
+    expect(cacheStore.deleteCallsCount).toBe(1);
+    expect(cacheStore.insertCallsCount).toBe(1);
+    expect(cacheStore.insertKey).toBe("purchases");
   });
 });
