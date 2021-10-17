1516764780c16587ee68e54a592866676f540414
will-cesar <willcesar010421@gmail.com>
2021-10-16 21:06:46 -0300

    ensure LocalLoadPurchases returns empty list on load failure

--- src/data/usecases/load-purchases/local-load-purchases.spec.ts
+++ src/data/usecases/load-purchases/local-load-purchases.spec.ts
@@ -24,9 +24,36 @@ describe("LocalLoadPurchases", () => {
   });
 
   test("Should call correct key on load", async () => {
+    /**
+     * Teste para garantir que o loadAll() está sendo chamado pelo método correto,
+     * e que está sendo chamada a key correta
+     */
+
     const { cacheStore, sut } = makeSut();
     await sut.loadAll();
     expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.fetch]);
     expect(cacheStore.fetchKey).toBe("purchases");
+  });
+
+  test("Should return empty list if load fails", async () => {
+    /**
+     * Teste para garantir que seja retornada uma lista vazia caso o
+     * método loadAll() falhe
+     *
+     * - Se espera que primeiro chame o método fetch() do CacheStore e falhe,
+     * logo em seguida chame o método delete()
+     * - Se espera que a key seja "purchases"
+     * - Se espera que o retorno do método loadAll() seja um array vazio
+     */
+
+    const { cacheStore, sut } = makeSut();
+    cacheStore.simulateFetchError();
+    const purchases = await sut.loadAll();
+    expect(cacheStore.actions).toEqual([
+      CacheStoreSpy.Action.fetch,
+      CacheStoreSpy.Action.delete,
+    ]);
+    expect(cacheStore.deleteKey).toBe("purchases");
+    expect(purchases).toEqual([]);
   });
 });
