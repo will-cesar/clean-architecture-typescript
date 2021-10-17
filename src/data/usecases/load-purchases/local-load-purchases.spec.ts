84d459c1af19a74811d67553301c71a930120c0c
will-cesar <willcesar010421@gmail.com>
2021-10-16 22:46:15 -0300

    ensure LocalLoadPurchases has no side effects on load if it fails

--- src/data/usecases/load-purchases/local-load-purchases.spec.ts
+++ src/data/usecases/load-purchases/local-load-purchases.spec.ts
@@ -38,11 +38,7 @@ describe("LocalLoadPurchases", () => {
     cacheStore.simulateFetchError();
     const purchases = await sut.loadAll();
 
-    expect(cacheStore.actions).toEqual([
-      CacheStoreSpy.Action.fetch,
-      CacheStoreSpy.Action.delete,
-    ]);
-    expect(cacheStore.deleteKey).toBe("purchases");
+    expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.fetch]);
     expect(purchases).toEqual([]);
   });
 
